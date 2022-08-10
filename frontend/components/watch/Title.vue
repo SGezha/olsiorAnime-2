590f60ecd7dc7cb545d142117292c94ee5afa8f9
Sasha <dr.uma228@hotmail.com>
2022-08-10 16:04:48 +0300

    components

---
+++ frontend/components/watch/Title.vue
@@ -0,0 +1,24 @@
+<script setup>
+const getSecondTitle = (data) => {
+    return `${data.title.split('/')[1]}, ${new Date(data.date).toLocaleDateString()}, `
+}
+
+defineProps({
+    anime: {
+        type: Object,
+        default: () => {}
+    }
+})
+</script>
+
+<template>
+<h1 class="font-bold text-2xl">
+    {{ anime.attributes.title.split('/')[0] }}
+</h1>
+<h2 class="font-regular opacity-70 text-xl">
+    {{ getSecondTitle(anime.attributes) }} {{ `${anime.attributes.maxepisodes == 1 ?
+            $t('film') : anime.attributes.nowepisode + ' ' + $t('iz') + ' ' +
+            anime.attributes.maxepisodes + ' ' + $t('ep')}`
+    }}
+</h2>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -6,6 +6,6 @@ const { pending, data: animes } = useAsyncData('animes', () => find('animes', { 
 
 <template>
     <div v-if="!pending" class="grid w-100 p-[10px] py-[20px] gap-[30px] xl:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 mx-auto max-w-[1350px]">
-        <myCard v-for="anime in animes.data" :key="anime.id" :card="anime.attributes" :id="anime.id" />
+        <MyCard v-for="anime in animes.data" :key="anime.id" :card="anime.attributes" :id="anime.id" />
     </div>
 </template>
