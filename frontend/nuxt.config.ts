1bd48858de9639c71046b3c45b2a1f03986cc1c8
Sasha <dr.uma228@hotmail.com>
2022-08-12 17:45:57 +0300

    fix seo

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,9 +4,8 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/strapi',
         '@vueuse/nuxt',
-        '@pinia/nuxt',
         '@nuxtjs/tailwindcss',
-        '@nuxtjs/color-mode'
+        '@nuxtjs/color-mode',
     ],
     strapi: {
         url: process.env.API,
@@ -35,18 +34,14 @@ export default defineNuxtConfig({
             meta: [
                 { charset: 'utf-8' },
                 { 'http-equiv': 'cache-control', content: 'max-age=31536000' },
-                { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1"' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
                 { name: 'yandex-verification', content: '9ef2af352f0e448e' },
-                { hid: 'description', name: 'description', content: 'Сайт с записями стримов просмотра аниме Olsior\'а! С удобным интерфейсом и крутыми фишками. Сохранение прогресса просмотра, запись чата.' },
-                { property: 'og:description', hid: 'og:description', name: 'og:description', content: 'Сайт с записями стримов просмотра аниме Olsior\'а! С удобным интерфейсом и крутыми фишками. Сохранение прогресса просмотра, запись чата.' },
-                { property: 'og:title', hid: 'og:title', name: 'og:title', content: 'Олсиор смотрит аниме' },
-                { property: 'og:url', hid: 'og:url', name: 'og:url', content: 'https://smotrel.net' },
-                { property: 'og:image', hid: 'og:image', name: 'og:image', content: 'https://smotrel.net/site.jpg' },
                 { hid: 'Keywords', name: 'Keywords', content: 'Olsior, olsior, олсиор, Олсиор, смотрит, Смотрит, аниме, Аниме, Olsior смотрит аниме, Olsior записи аниме стримов, олсиор аниме записи, Anime, anime, наруто, блич, Naruto, bleach, Bleach, врата штейна, смотрит, аниме, записи стримов, стримы, Стримы олсиора, Записи олсиора, Olsior twitch, олсиор твитч, смотреть аниме с олсиором, смореть аниме' },
                 { name: 'format-detection', content: 'telephone=no' }
             ],
             link: [
                 { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                // { rel: 'manifest', href: '/manifest.webmanifest' },
             ],
         },
     }
