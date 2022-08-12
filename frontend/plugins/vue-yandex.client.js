fd52cc140d3e9fd498c3b4687a4b111cfe931721
Sasha <dr.uma228@hotmail.com>
2022-08-12 22:08:56 +0300

    yandex fix

--- frontend/plugins/vue-yandex.client.js
+++ frontend/plugins/vue-yandex.client.js
@@ -1,11 +1,11 @@
 import VueYandexMetrika from 'vue-yandex-metrika'                               
 
 export default defineNuxtPlugin((nuxtApp) => {
-    // nuxtApp.vueApp.use(VueYandexMetrika, {
-    //     id: 84051220,
-    //     env: "production",
-    //     webvisor: true,
-    //     clickmap: true
-    //     // other options
-    // })
+    nuxtApp.vueApp.use(VueYandexMetrika, {
+        id: 84051220,
+        env: "production",
+        webvisor: true,
+        clickmap: true
+        // other options
+    })
 })
