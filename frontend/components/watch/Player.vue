28125dccb0ffd914a9ba7969ba66e75c7f1a7ff0
Sasha <dr.uma228@hotmail.com>
2022-08-13 06:26:19 +0300

    fix speed

--- frontend/components/watch/Player.vue
+++ frontend/components/watch/Player.vue
@@ -452,7 +452,7 @@ const toggleFullScreen = () => {
                                                     @change="changeEpisode(current, true, currentTime)"
                                                     v-model="heroku">
                                             </VideoMenuItem>
-                                            <VideoMenuItem v-if="anime.attributes.episodes[current].chat != undefined">
+                                            <VideoMenuItem>
                                                 <svg xmlns="http://www.w3.org/2000/svg"
                                                     xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
                                                     role="img" class="iconify iconify--carbon" width="24" height="24"
