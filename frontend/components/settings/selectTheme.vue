9fea3e70487f0cfadf93abe2f7abecc6e50e37ac
Sasha <dr.uma228@hotmail.com>
2022-08-15 00:11:37 +0300

    fix

--- frontend/components/settings/selectTheme.vue
+++ frontend/components/settings/selectTheme.vue
@@ -7,7 +7,6 @@
 </template>
 
 <script setup>
-const colorMode = useColorMode()  
 const themes = useState(() => ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"])
 const changeTheme = (event) => {
   localStorage.setItem('nuxt-color-mode', event.target.value)
