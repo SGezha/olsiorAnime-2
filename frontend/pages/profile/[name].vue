eb5c0a72852995f04bb69ecbd3998a896f92838b
Sasha <dr.uma228@hotmail.com>
2022-08-14 23:51:58 +0300

    fix profile

--- frontend/pages/profile/[name].vue
+++ frontend/pages/profile/[name].vue
@@ -165,16 +165,6 @@ const saveBg = async () => {
                         <div class="pt-[10px] text-neutral-content block w-[90%] break-words">
                             {{ com.attributes.msg }}
                         </div>
-                        <button v-if="com.attributes.author == twName" @click="deleteCom(com.id)"
-                            class="absolute w-[30px] h-[30px] p-[5px] bg-error flex items-center justify-center rounded-full text-white right-[20px] bottom-[20px]">
-                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                aria-hidden="true" role="img" class="iconify iconify--mdi"
-                                preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-                                <path fill="currentColor"
-                                    d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12Z">
-                                </path>
-                            </svg>
-                        </button>
                     </div>
                 </div>
             </div>
