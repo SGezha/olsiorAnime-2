f9bee2ad6e7aef8aedd07eb20b17b7ec2ad95daa
Sasha <dr.uma228@hotmail.com>
2022-08-15 17:52:13 +0300

    bug report

--- frontend/pages/watch/[name].vue
+++ frontend/pages/watch/[name].vue
@@ -66,6 +66,12 @@ const { isFullscreen, enter, exit, toggle } = useFullscreen(page)
 
                         <WatchInfo v-if="!isFullscreen" :anime="anime.data[0]" />
 
+                        <div class="mt-[15px]">
+                            <div class="text-2xl font-bold text-error">{{ $t('warning') }}</div>
+                            <div class="mt-[10px]">{{ $t('dontep1') }}<span class="text-error">{{ $t('dontep2') }}</span> {{ $t('dontep3') }} </div>
+                            <div class="">{{ $t('bugreport') }} <a href="https://t.me/FuNSasha" class="link">SGezha</a>.</div>
+                        </div>
+
                         <WatchComents v-if="!isFullscreen" :url="router.currentRoute._value.params.name" />
                     </div>
                 </div>
