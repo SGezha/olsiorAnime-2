1b553b931425d3b359a651c4bca9957cac83f32b
Sasha <dr.uma228@hotmail.com>
2022-08-22 10:57:24 +0300

    fix text color

--- frontend/components/watch/AnimeInfo.vue
+++ frontend/components/watch/AnimeInfo.vue
@@ -14,22 +14,22 @@ defineProps({
 <template>
 <div class="px-[12px] py-[16px] mt-[20px] w-full text-sm bg-neutral shadow-lg rounded-md flex flex-col gap-[5px]">
     <div class="text-neutral-content">
-        <span>{{ $t('status') }}: </span>
-        <span class="text-primary font-bold">{{ anime.attributes.watched ? $t('watched') : $t('watching') }}</span>
+        <span class="opacity-60">{{ $t('status') }}: </span>
+        <span class="font-bold">{{ anime.attributes.watched ? $t('watched') : $t('watching') }}</span>
     </div>
     <div class="text-neutral-content">
-        <span>{{ $t('epwached') }}: </span>
-        <span class="text-primary font-bold">{{ `${anime.attributes.maxepisodes == 1 ?
+        <span class="opacity-60">{{ $t('epwached') }}: </span>
+        <span class="font-bold">{{ `${anime.attributes.maxepisodes == 1 ?
             $t('film') : anime.attributes.nowepisode + ' ' + $t('iz') + ' ' +
             anime.attributes.maxepisodes + ' ' + $t('ep')}`}}</span>
     </div>
     <div class="text-neutral-content">
-        <span>{{ $t('watcheddate') }}: </span>
-        <span class="text-primary font-bold">{{ new Date(anime.attributes.date).toLocaleDateString() }} </span>
+        <span class="opacity-60">{{ $t('watcheddate') }}: </span>
+        <span class="font-bold">{{ new Date(anime.attributes.date).toLocaleDateString() }} </span>
     </div>
     <div class="text-neutral-content">
-        <span>{{ $t('genres') }}: </span>
-        <span class="text-primary font-bold">{{ anime.attributes.genres.toString().split(',').join(', ') }} </span>
+        <span class="opacity-60">{{ $t('genres') }}: </span>
+        <span class="font-bold">{{ anime.attributes.genres.toString().split(',').join(', ') }} </span>
     </div>
 </div>
 </template>
