e63d0a96f2655923dabbb6628829bd0ffc0bbcf2
Sasha <dr.uma228@hotmail.com>
2022-09-20 11:35:06 +0300

    default dark theme -> night

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     plugins: [require('daisyui')],
     daisyui: {
         styled: true,
-        themes: ["light", 'dark', "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+        themes: ["light", {
+            dark: {
+                ...require("daisyui/src/colors/themes")["[data-theme=night]"],
+            }
+        }, "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "coffee", "winter"],
         base: true,
         utils: true,
         logs: true,
