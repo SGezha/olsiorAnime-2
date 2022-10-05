5b50972a1e42741015de68b1b9f6d981bf93ed78
Sasha <dr.uma228@hotmail.com>
2022-10-05 12:25:08 +0300

    fix yandex

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
             meta: [
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
-                { name: 'yandex-verification', content: '9ef2af352f0e448e' },
+                { name: 'yandex-verification', content: '2c14acb80e12a24e' },
                 { hid: 'Keywords', name: 'Keywords', content: 'Olsior, olsior, олсиор, Олсиор, смотрит, Смотрит, аниме, Аниме, Olsior смотрит аниме, Olsior записи аниме стримов, олсиор аниме записи, Anime, anime, наруто, блич, Naruto, bleach, Bleach, врата штейна, смотрит, аниме, записи стримов, стримы, Стримы олсиора, Записи олсиора, Olsior twitch, олсиор твитч, смотреть аниме с олсиором, смореть аниме' },
                 { name: 'format-detection', content: 'telephone=no' }
             ],
