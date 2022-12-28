366dec591506bd7d9067fec75c6cdf9e8ad0da54
Sasha <dr.uma228@hotmail.com>
2022-12-28 11:46:28 +0300

    fix link

--- frontend/components/watch/Player.vue
+++ frontend/components/watch/Player.vue
@@ -325,8 +325,8 @@ const keyHandler = (e) => {
                             class="chat p-[10px] overflow-y-auto overflow-x-hidden flex flex-col relative transition-all right-0"
                             :style="{ width: `${chatShow && !chatPip ? chatSize : 0}%`, display: `${chatShow && !chatPip ? 'flex' : 'none'}` }">
                             <div v-for="msg in nowChat" :key="msg" class="block w-100">
-                                <span @click="openLink(`https://smotrel.net/profile/${msg.author}`)"
-                                    class="font-bold cursor-pointer" :style="{ color: msg.color }">{{ msg.author
+                                <span 
+                                    class="font-bold" :style="{ color: msg.color }">{{ msg.author
                                     }}</span>:
                                 <span class="text-white" v-html="msg.text"></span>
                             </div>
